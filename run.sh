a1f7dbcfc132f5edaf54f26ee5b82b750fa752ec
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-12-28 00:37:32 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -o errexit
+
+echo "$1" | docker login docker.pkg.github.com -u "$GITHUB_ACTOR" --password-stdin
+
+IMAGE_NAME=$(basename "$GITHUB_REPOSITORY")
+IMAGE_ID="docker.pkg.github.com/$GITHUB_REPOSITORY/$IMAGE_NAME"
+VERSION=$(basename "$GITHUB_REF")
+
+if [ "$VERSION" == "master" ]
+then
+    VERSION=latest
+fi
+
+docker tag image "$IMAGE_ID:$VERSION"
+docker push "$IMAGE_ID:$VERSION"
